3a5aa7e0f67558549d50d6f278703e871279b935
sssubik <subikshrestha@gmail.com>
2021-07-06 23:41:04 +0545

    changed contract address

--- scripts/contract_addresses.py
+++ scripts/contract_addresses.py
@@ -7,7 +7,7 @@ CONTRACTS = {
     },
     "rinkeby": {
         "tokenart1":"0x2Fc8D92319a4973B26f2830E1d0324Fef477363f",
-        "tokenart2":"0xb01D5487C9CDd711305Bc4F12c99f07d52fe47D5"
+        "tokenart2":"0xE08C78cFFC4D38a207c12330bfb8375206551796"
     },
     "kovan": {
         "tokenart":""
